1e1a343c55b85c4ab40e124e1da704ec2e0d7c89
ankit-raj00 <ankitkumar5259211@gmail.com>
2025-12-10 18:54:08 +0530

    deploy: fix deployment issue

--- main.py
+++ main.py
@@ -33,7 +33,11 @@ app = FastAPI(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "https://chatbot-backend-beta-nine.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
