5497ae4910edaf0eb7c5714ec17ef32ca57cba03
ankit-raj00 <ankitkumar5259211@gmail.com>
2025-12-18 23:03:56 +0530

    Refactor resource handling and update docstrings

--- main.py
+++ main.py
@@ -37,9 +37,9 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://localhost:5173",
+
         "https://chatbot-backend-beta-nine.vercel.app"
+        "https://chatbot-khaki-eta-53.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
